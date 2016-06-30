1d485cd3c6e2ddb4b83e64c2aad44581a6b20906
Jacob Gray <jacob@jacobgray.me>
2016-06-30 09:23:00 -0400

    Update and rename SOCVR-Alert.0.5.user.js to SOCVR-Alert.0.6.user.js

--- SOCVR-Alert.0.6.user.js
+++ SOCVR-Alert.0.6.user.js
@@ -10,6 +10,8 @@
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_notification
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @grant        Notifications
 // ==/UserScript==
 
@@ -23,9 +25,8 @@
         },
         currentVersion = 0.5,
         updateURL = "https://raw.githubusercontent.com/Jacob-Gray/SE-Userscripts/master/current/SOCVR-Alert.update.user.js",
-        checkUpdateURL = "https://raw.githubusercontent.com/Jacob-Gray/SE-Userscripts/master/current/SOCVR-Alert.update.version",
-        styles = ".socvr-alert-snackbar{position:fixed;display:none;z-index:100;top:0;left:0;right:0;background:#305d5d;font-size: 110%;color:white;text-align:center;padding:10px;font-family:inherit;}";
-
+        checkUpdateURL = "https://raw.githubusercontent.com/Jacob-Gray/SE-Userscripts/master/current/SOCVR-Alert.update.json",
+        styles = ".socvr-alert-dialog,.socvr-alert-snackbar{position:fixed;display:none;top:0;left:0;right:0}.socvr-alert-snackbar{z-index:100;cursor:pointer;background:#305d5d;font-size:110%;color:#fff;text-align:center;font-family:inherit;padding:10px}.socvr-alert-dialog{bottom:0;z-index:99;justify-content:center;align-items:center}.socvr-alert-dialog.active{display:flex!important}.socvr-alert-dialog-back{position:absolute;top:0;left:0;right:0;bottom:0;opacity:.5;background:linear-gradient(to left,#485563,#29323c)}.socvr-alert-dialog-con{max-width:600px;opacity:0;position:relative;background:#fff;transform:translateY(-50px);transition:.3s;margin:auto;border-radius:2px;box-shadow:0 0 2px rgba(0,0,0,.2),0 2px 1px rgba(0,0,0,.2);padding:10px}.socvr-alert-dialog-con ol{padding-left:15px}.socvr-alert-dialog-con.active{transform:none;opacity:1}.btn{background:#13a38d;border-radius:2em;color:#FFF;cursor:pointer;font-size:90%;padding:4px 7px}.btn:hover{background:#41af9e}";
     function watch(el, isMsgList){
         isMsgList = isMsgList || false;
 
@@ -39,15 +40,18 @@
     }
 
     function checkForUpdates(){
-        $.ajax({
-            url: checkUpdateURL,
-            success: function(data){
-                if(data > currentVersion) snackbar("SOCVR Alert Version "+data+" is published! Click here to update.", function(){
-                    document.location = updateURL;
-                    $(".socvr-alert-snackbar").html("Click here to reload the page after updating").off("click").on("click",function(){
-                        document.location.reload();
-                    });
+        $.getJSON( checkUpdateURL, function( data ) {
+            if(data.version > currentVersion) snackbar("SOCVR Alert [Update] "+data.version+" - "+data.summary+". Click here to update", function(){
+                document.location = updateURL;
+                $(".socvr-alert-snackbar").html("Click here to reload the page after updating").off("click").on("click",function(){
+                    document.location.reload();
                 });
+            });
+            else {
+                if(GM_getValue("socvr-alert-version") < currentVersion || GM_getValue("socvr-alert-version") === undefined){
+                    dialog("<p>"+data.summary+"</p><p><b>Changelog</b>:"+listify(data.changes)+"</p>","Updated SOCVR-Alert to Ver. "+data.version);
+                    GM_getValue("socvr-alert-version", currentVersion);
+                }
             }
         });
     }
@@ -72,10 +76,16 @@
             });
         }
 
-        checkForUpdates();
-
         $("<style></style>").html(styles).appendTo("body");
-        $("body").append("<div class='socvr-alert-snackbar'></div>");
+        $("body").append("<div class='socvr-alert-snackbar'></div>").append("<div class='socvr-alert-dialog'><div class='socvr-alert-dialog-back'></div><div class='socvr-alert-dialog-con'></div></div>");
+
+        $(".socvr-alert-dialog-back").on("click",function(){
+            $(this).siblings().removeClass("active").parent().fadeOut(function(){
+                $(this).removeClass("active");
+            });
+        });
+
+        checkForUpdates();
 
         try{
             if(setUp) watch(chat);
@@ -160,8 +170,29 @@
         else if(typeof duration == "function") bar.on("click", duration);
     }
 
+    function dialog(text, title){
+        var dialog = $(".socvr-alert-dialog"),
+            con = dialog.find(".socvr-alert-dialog-con");
+        con.html("<h3>"+title+"</h3><p>"+text+"</p>");
+        dialog.fadeIn(function(){
+            $(con).addClass("active");
+        }).addClass("active");
+    }
+
     function getText(msg){
         return $(".content", msg).clone().children().remove().end().text().replace(/^ */g,"");
     }
+    function listify(text){
+        var ret = text.split(/[,;.]/).map(function(i){
+            return (i!=="")?"<li>"+i.capitalizeFirstLetter()+".</li>":i;
+        });
+        ret.push("</ol>");
+        ret.unshift("<ol>");
+        return ret.join("");
+    }
+
+    String.prototype.capitalizeFirstLetter = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    };
 
 })();
