ac8e2db0c540da6d6301a22dffd0dee5bed30126
Jacob Gray <jacob@jacobgray.me>
2016-07-01 12:22:19 -0400

    Create chat-styles.1.user.js

---
+++ chat-styles/chat-styles.1.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Chat-styles
+// @namespace    https://github.com/Jacob-Gray/SE-Userscripts/
+// @version      1
+// @description  Change the default theme of the SE chats
+// @author       Jacob Gray
+// @match        http://chat.stackoverflow.com/*
+// @match        http://chat.stackexchange.com/*
+// @match        http://chat.meta.stackexchange.com/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $("head").append("<link rel=stylesheet href=https://raw.githubusercontent.com/Jacob-Gray/SE-Userscripts/master/chat-styles/chat-styles.css>");
+})();
